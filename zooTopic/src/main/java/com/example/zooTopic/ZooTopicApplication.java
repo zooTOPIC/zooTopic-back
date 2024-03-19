68ee9144eb691717493d18dc8a16759d617aafbc
seoyeoneee22 <nsy1382@naver.com>
2024-03-19 10:08:15 +0900

    plz initial settings

---
+++ zooTopic/src/main/java/com/example/zooTopic/ZooTopicApplication.java
@@ -0,0 +1,13 @@
+package com.example.zooTopic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooTopicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooTopicApplication.class, args);
+	}
+
+}
