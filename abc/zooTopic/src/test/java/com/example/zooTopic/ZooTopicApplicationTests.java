1ad94dfa56f6e5a8ced287e2095bd89ffc3a3208
seoyeoneee22 <88606675+seoyeoneee22@users.noreply.github.com>
2024-03-19 10:34:01 +0900

    initial settings

---
+++ abc/zooTopic/src/test/java/com/example/zooTopic/ZooTopicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zooTopic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooTopicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
